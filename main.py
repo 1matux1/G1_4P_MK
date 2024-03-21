908d7e99be74cf7fae6868ead8107ad8988167ea
1matux1 <mateusz.kmak@onet.pl>
2024-03-21 10:02:33 +0100

    Nowy algorytm
    Dodanie sortowania szybkiego

--- main.py
+++ main.py
@@ -6,7 +6,17 @@ def bubble_sort(arr):
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
 
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
 arr = [64, 34, 25, 12, 22, 11, 90]
 print("Sortowanie bąbelkowe:")
 print(bubble_sort(arr.copy()))
-
+print("Sortowanie szybkie:")
+print(quick_sort(arr.copy()))
